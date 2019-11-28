e67ef5ad9e8c50c0d804a44b0a2bb8de0def5d77
WilliamChan <wchan@lcs.on.ca>
2019-11-28 16:19:19 -0500

    finished loop/switch statement

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: propertites
+    @IBOutlet weak var messageInput: UITextView!
+    @IBOutlet weak var messageOutput: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func convert(_ sender: Any) {
+        
+    
+        //INPUT
+        
+        //guard statement
+        guard let message = messageInput.text, message.count>0 else{
+            messageOutput.text = "Please enter phone number to convert."
+            return
+        }
+        //PROCESS & OUTPUT
+        var output = ""
+        for character in message {
+            switch character {
+            case "1":
+                output += "1"
+            case "A","B","C","2":
+                output += "2"
+            case "D","E","F","3":
+                output += "3"
+            case "G","H","I","4":
+                output += "4"
+            case "J","K","L","5":
+                output += "5"
+            case "M","N","O","6":
+                output += "6"
+            case "P","Q","R","S","7":
+                output += "7"
+            case "T","U","V","8":
+                output += "8"
+            case "W","X","Y","Z","9":
+                output += "9"
+            case "0":
+                output += "0"
+            default:
+                output += "-"
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
 }
-
