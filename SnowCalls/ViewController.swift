40a306f06210202f08278874522738135fae4b41
WilliamChan <wchan@lcs.on.ca>
2019-11-28 14:41:15 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by William Chan on 2019-11-28.
+//  Copyright © 2019 WilliamChan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
